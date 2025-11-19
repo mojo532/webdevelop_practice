689c2aa4d9eb031f22bad70fef97fc7c1a323054
Kyungmin Kim <rudals5320@gmail.com>
2025-11-19 21:00:23 +0100

    Day 30

---
+++ js_practice/app.js
@@ -0,0 +1,7 @@
+let userAge = 30;
+let userName = "Alpaka";
+let greetingText = "Hi I am Alpaka";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = { title: "Developer", place: "Berlin", salary: 50000 };
+
+alert(hobbies[0]);
