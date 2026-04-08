9e243ab2f42730639e3cab75e77e8049112bc66f
Kyungmin Kim <rudals5320@gmail.com>
2026-04-08 20:33:52 +0200

    Day 31

--- js_practice/app.js
+++ js_practice/app.js
@@ -2,6 +2,19 @@ let userAge = 30;
 let userName = "Alpaka";
 let greetingText = "Hi I am Alpaka";
 let hobbies = ["Sports", "Cooking", "Reading"];
-let job = { title: "Developer", place: "Berlin", salary: 50000 };
+let job = {
+  title: "Developer",
+  place: "Berlin",
+  salary: 50000,
+};
 
-alert(hobbies[0]);
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+  return userAge - 18;
+}
+
+age = 50;
+totalAdultYears = calculateAdultYears(age);
+
+alert(totalAdultYears);
